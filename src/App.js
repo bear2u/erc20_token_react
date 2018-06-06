6b0ae0bf68d5f972d48bd140231501f1b84cd2d0
bear2u <bear2u@naver.com>
2018-06-06 12:09:28 +0900

    add submit form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import CustomTokenContract from "../build/contracts/CustomToken.json";
 import getWeb3 from "./utils/getWeb3";
+import { Label, Button, Checkbox, Form } from "semantic-ui-react";
 
-const contract = require('truffle-contract');
+const contract = require("truffle-contract");
 
 class App extends Component {
   constructor(props) {
@@ -11,12 +12,21 @@ class App extends Component {
     this.state = {
       storageValue: 0,
       web3: null,
-      isAttached : false
+      isAttached: false,
+      account: "",
+      balance: 0,
+
+      tokenSymbol: "",
+      tokenName: "",
+      tokenDecimals: 0,
+      tokenTotalSupply: 0
     };
+
+    this.handleEvent = this.handleEvent.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  async componentWillMount() {
-
+  async componentDidMount() {
     // const React = require('react');
 
     // //It should be 16.3.2
@@ -28,44 +38,130 @@ class App extends Component {
     });
 
     const accounts = await this.state.web3.eth.getAccounts();
-    
+    console.log(accounts);
+
+    this.setState({
+      account: accounts[0]
+    });
+
     await this.instantiateContract();
   }
 
-  async instantiateContract() {    
+  //https://goo.gl/QH3EX8
+  async instantiateContract() {
     const customToken = contract(CustomTokenContract);
     customToken.setProvider(this.state.web3.currentProvider);
 
-    try{
+    try {
       const tokenContract = await customToken.deployed();
+      this.customTokenInstance = tokenContract;
+      const balance = await this.customTokenInstance.balanceOf(
+        this.state.account
+      );
 
-      if(tokenContract != null) {      
+      console.dir(this.customTokenInstance);
+      this.address = this.customTokenInstance.address;
+
+      this.setState({
+        balance: balance.toNumber()
+      });
+
+      if (tokenContract != null) {
         this.setState({
-          isAttached : true
-        })
+          isAttached: true
+        });
       }
 
       console.log(tokenContract);
-
-    } catch( e ) {
-      alert('계약 배포가 안되어있음 , 서버가 다를수 있음');
+    } catch (e) {
+      alert("계약 배포가 안되어있음 , 서버가 다를수 있음");
     }
+  }
 
-    /*
-    1. 생성
-    2. 특정 계정에게 토큰 전달
-    3. 승인후에 토큰 전달
-    4. 잔액체크
-    */
-    
+  handleEvent(e) {
+    const name = e.target.name;
+
+    console.log(e.target.value);
+    this.setState({
+      [name]: e.target.value
+    });
+  }
+
+  async handleSubmit(e) {
+    e.preventDefault();
+    const tokenSymbol = this.state.tokenSymbol;
+    const tokenName = this.state.tokenName;
+    const tokenDecimals = this.state.tokenDecimals;
+    const tokenTotalSupply = this.state.tokenTotalSupply;
+    console.log(tokenSymbol, tokenName, tokenDecimals, tokenTotalSupply);
+
+    await this.customTokenInstance.generate(
+      tokenSymbol,
+      tokenName,
+      tokenDecimals,
+      tokenTotalSupply,
+      {
+        from: this.state.account
+      }
+    );
+
+    const balance = await this.customTokenInstance.balanceOf(
+      this.state.account
+    );
+
+    this.setState({
+      balance
+    })
   }
 
   render() {
     return (
       <div>
-        배포상태 : { this.state.isAttached ? "ok" : "not connected" }
+        배포상태 : {this.state.isAttached ? "ok" : "not connected"} <br />
+        <Label>계정 주소</Label> {this.state.account} <br />
+        <Label>토큰 잔액 조회</Label> {this.state.balance} <br />
+        <Label>토큰 주소</Label> {this.address} <br />
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Field>
+            <label>심볼</label>
+            <input
+              placeholder="eg)txt"
+              name="tokenSymbol"
+              onChange={this.handleEvent}
+              value={this.state.tokenSymbol}
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>토큰이름</label>
+            <input
+              placeholder="eg)text"
+              name="tokenName"
+              onChange={this.handleEvent}
+              value={this.state.tokenName}
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>decimal</label>
+            <input
+              placeholder="2"
+              name="tokenDecimals"
+              onChange={this.handleEvent}
+              value={this.state.tokenDecimals}
+            />
+          </Form.Field>
+          <Form.Field>
+            <label>총수량</label>
+            <input
+              placeholder="10000"
+              name="tokenTotalSupply"
+              onChange={this.handleEvent}
+              value={this.state.tokenTotalSupply}
+            />
+          </Form.Field>
+          <Button type="submit">Submit</Button>
+        </Form>
       </div>
-    )
+    );
   }
 }
 
--- truffle.js
+++ truffle.js
@@ -1,9 +1,22 @@
+// var HDWalletProvider = require("truffle-hdwallet-provider");
+
+// var mnemonic = "art include push idle erosion switch kingdom switch device hamster expose craft";
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
+      port: 9545,
+      network_id: "*" // Match any network id
+    },
+    ganache: {
+      host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
     }
+    // ganache2: {
+    //   provider: new HDWalletProvider(mnemonic, "http://127.0.0.1:7545"),
+    //   network_id: "*"
+    // }
   }
 };
